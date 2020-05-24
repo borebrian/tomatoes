5bc07bd5cced2104a3cb0d78f524bf5e52be0944
K-HUB  LATITUDE <borebrian77@gmai.com>
2020-05-24 12:36:11 -0700

    Flex and expanded

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
@@ -18,51 +19,48 @@ class Home extends StatelessWidget{
     appBar: AppBar(
     centerTitle: true,
     backgroundColor: Colors.red[900],
-    title: const Text('Tomatoes',
+    title: const Text('Mandazi',
     style: TextStyle(
     fontFamily: 'fontcustom',
     ),),
 
     ),
-    body: Center(
-        child: RaisedButton(
-            onPressed: () => {},
-            textColor: Colors.white,
-            color: Colors.red[900],
-          highlightColor: Colors.red[400],
-            child: Text(
-              'Click Me',
-              style: TextStyle(fontSize: 20),
-            ),),
+    body: Row(
+      children: <Widget>[
 
+Expanded(
+  flex:10,
+       child: Container(
+          color: Colors.yellow,
+          padding:EdgeInsets.all(10.0),
+          child: Text("dcfjkghdfjkgh"),
+       ),
+        ),
+        Expanded(
+          flex: 10,
+          child: Container(
+color: Colors.green,
+  padding:EdgeInsets.all(10),
+  child: Text("dcfjkghdfjkgh"),
 
+),
+        ),
+        Expanded(
+          flex: 10,
+child:
+        Container(
 
+          color: Colors.red[900],
+          padding:EdgeInsets.all(10),
+          child: Text("dcfjkghdfjkgh"),
 
-//      child: Icon(
-//        Icons.account_circle,
-//        color: Colors.red[900],
-//        size: 90,
-//      ),
-//      child:Image(
-//        image: AssetImage("images/tomatoes2.png"),
-//
-//      ),
+        ),
+        ),
 
-//    child: Text("Tomatoes"
-//    ,style: TextStyle(
-//    fontWeight: FontWeight.bold,
-//    color: Colors.red[900],
-//    fontSize: 40,
-//    fontFamily: 'fontcustom',
-//    ),
-
-
-    ),floatingActionButton: FloatingActionButton(
-    child:Text("Add"),
-onPressed: (){},
-      backgroundColor: Colors.red[900],
+      ],
     ),
     );
+
   }
 
 }
